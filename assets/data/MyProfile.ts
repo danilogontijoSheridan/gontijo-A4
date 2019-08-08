979a52557b7fbd175076ca70d41defeb8ba4a2cb
danilogontijoSheridan <52791736+danilogontijoSheridan@users.noreply.github.com>
2019-08-08 11:13:56 -0400

    Add files via upload

---
+++ assets/data/MyProfile.ts
@@ -0,0 +1,10 @@
+import {Gontijo} from "../../app/gontijo";
+
+export const MYINFO : Gontijo = {
+
+    sNumber : 9991492098,
+    sName : 'Danilo Gontijo',
+    sLogin : 'gontijo',
+    sCampus :'Oakville',
+    sTitle : 'Assignment 4'
+};
